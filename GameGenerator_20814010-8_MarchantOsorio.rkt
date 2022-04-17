3d9962ef83c5384a397a82bd41671d633b3cf3ca
Cristobal Marchant <cristobal.marchant@usach.cl>
2022-04-17 14:19:52 -0400

    Definiciones listas

--- GameGenerator_20814010-8_MarchantOsorio.rkt
+++ GameGenerator_20814010-8_MarchantOsorio.rkt
@@ -5,20 +5,20 @@
 ;tda->player (nombre turno puntos)
 
 ;TDA game - constructor---------------------------------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion player
+;Dominio: Entero
+;Recorrido:  Lista
+;Descripcion: definine la estructura para cada jugador
+;Tipo: Cola
 (define (player n)
   (cond
     [(= n 0) null]
     [else (cons (list n 0 0) (player (- n 1)))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
+;Funcion game
+;Dominio: Entero X Lista X Funcion X Booleano
+;Recorrido: Lista de Listas
+;Descripcion: genera el juego con el numero de jugadores, el mazo, el modo y si se quiere azar o no.
 ;Tipo: 
 (define game (lambda (Np L Modo FR)
                (cond
@@ -26,11 +26,11 @@
                  [else (cons (player Np) (Modo L))])))
 
 ;TDA game - stackMode----------------------------------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion stackMode
+;Dominio: Lista
+;Recorrido: Listas
+;Descripcion: genera el juego de stack mode, una lista con 2 cartas y el resto del mazo.
+;Tipo: Declarativa
 (define (stackMode L)
   (cond
     [(empty? L) null]
@@ -38,11 +38,11 @@
     [else (list (list (encontrar L (length L)) (encontrar L (-(length L)1))) (eleminar (eleminar L (length L)) (-(length L)1)))]))
 
 ;TDA game - register-----------------------------------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion insertarRegister
+;Dominio: Lista X String
+;Recorrido: Lista
+;Descripcion: Agrega el nuevo nombre a la lista del game
+;Tipo: Natural
 (define (insertarRegister L e)
   (cond
     [(empty? L) null]
@@ -50,183 +50,185 @@
     [(number? (car(car L))) (cons (list e 0 0) (cdr L))]
     [else (cons (car L) (insertarRegister (cdr L) e))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo:     
+;Funcion register
+;Dominio: String X Lista
+;Recorrido: Lista de Listas
+;Descripcion: agrega la lista con los nuevos nombres a el game
+;Tipo: Declarativa     
 (define (register name L)
     (cons (insertarRegister (car L) name) (cdr L)))
 
 ;TDA game - whoseTurnIsIt?-----------------------------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion turno?
+;Dominio: Lista X Lista
+;Recorrido: String
+;Descripcion: dice de quien es el turno, bajo la logica si todos estan iguales el turno es para el primero
+              ;en otro caso si el anterior es mayor que el siguiente, el turno es para el siguiente.
+;Tipo: Cola
 (define (turno? L L1)
   (cond
-    [(empty? (cdr L)) (car(car L1))]
-    [(equal? (car(cdr(car L))) (car(cdr(car(cdr L))))) (turno? (cdr L) L1)]
-    [(> (car(cdr(car L)))  (car(cdr(car(cdr L))))) (car(car(cdr L)))]))
+    [(empty? (cdr L)) (car(car L1))] ;cuando son todos iguales regresa el primero de la lista
+    [(equal? (car(cdr(car L))) (car(cdr(car(cdr L))))) (turno? (cdr L) L1)] ;caso cuando evalua igualdad
+    [(> (car(cdr(car L)))  (car(cdr(car(cdr L))))) (car(car(cdr L)))])) ;caso cuando unos es mayor que el otro
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
+;Funcion whoseTurnIsIt?
+;Dominio: Lista
+;Recorrido: String
+;Descripcion: Evalua la lista en el turno? y regresa un nombre
 ;Tipo:                  
 (define (whoseTurnIsIt? L)
   (turno? (car L) (car L)))
 
 ;TDA game - play---------------------------------------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;null-------------------------------------------------------------------------
+;Funcion nulo
+;Dominio: Lista
+;Recorrido: Lista
+;Descripcion: caso cuando la action de play es null, y regresa el estado del game
+;Tipo: Declarativa
 (define (nulo L)
   L)
 ;spotlt-----------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
-(define (aumentTurno L) ;car car L
+;Funcion aumentTurno 
+;Dominio: Lista
+;Recorrido: Lista
+;Descripcion: en la lista de jugadores aumenta el turno en 1 para la persona que este de turno
+;Tipo: Declarativa
+(define (aumentTurno L) 
   (cond
     [(empty? L) null]
     [else (append(list (car L) (+ (car(cdr L)) 1)) (cdr(cdr L)))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
-(define (aumentPunt L) ;car  car L
+;Funcion aumentPunt
+;Dominio: Lista
+;Recorrido: Lista
+;Descripcion: en la lista de jugadores aumenta el puntaje en 1 para la persona que este de turno
+;Tipo: Declarativa
+(define (aumentPunt L) 
   (cond
     [(empty? L) null]
     [else (list (car L) (car(cdr L)) (+(car(cdr(cdr L)))1))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion buscarSpotlt
+;Dominio: Lista X String
+;Recorrido: Booleano
+;Descripcion: Comprueba si el string esta dentro de las cartas volteadas
+;Tipo: Cola
 (define (buscarSpotlt L n) ; L n
   (cond
     [(empty? L) #false]
     [(equal? (car L) n) #true]
     [else (buscarSpotlt (cdr L) n)]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion GanoPuntos
+;Dominio: Lista
+;Recorrido: Lista (jugadores)
+;Descripcion: Si se ganaron puntos agrega puntos y suma un turno, sino solo suma un turno
+;Tipo: Cola
 (define (GanoPuntos L) ; car L
   (cond
     [(equal? (car(car L))(turno? L L)) (cons (aumentPunt(aumentTurno (car L))) (cdr L))]
     [else (cons (car L) (GanoPuntos (cdr L)))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion Spotlt2
+;Dominio: Lista X String
+;Recorrido: Lista
+;Descripcion: Comprueba si se ganaron puntos, sino pasa el turno
+;Tipo: Declarativa
 (define (Spotlt2 L n)
   (cond
     [(and(buscarSpotlt (car(car(cdr L))) n) (buscarSpotlt (car(cdr(car(cdr L)))) n)) (GanoPuntos (car L))]
     [else (pass1 (car L))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion SigTurn
+;Dominio: Lista
+;Recorrido: Lista
+;Descripcion: Elimina las cartas en juego, y agrega otras 2 cartas del mazo
+;Tipo: Declarativa
 (define (SigTurn L)
   (list (list (encontrar L (length L)) (encontrar L (-(length L)1))) (eleminar (eleminar L (length L)) (-(length L)1))))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion Spotlt3
+;Dominio: String X Lista
+;Recorrido: Lista (game)
+;Descripcion: Se ingresa el game y el elemento en comun, y se hace el proceso de SpotIt
+;Tipo: Declarativa
 (define (Spotlt3 n L)
   (cons (Spotlt2  L n) (stackMode (car(cdr(cdr L))))))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion spotIt
+;Dominio: String
+;Recorrido: String
+;Descripcion: Regresa el String ingresado
+;Tipo: Declarativa
 (define (spotIt n)
   n)
 
 ;pass-------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
+;Funcion pass1
+;Dominio: Lista
+;Recorrido: Lista(game)
+;Descripcion: aumenta en uno la cantidad de turnos del jugador actual
 ;Tipo: 
 (define (pass1 L)
   (cond
     [(equal? (car(car L))(turno? L L)) (cons (aumentTurno (car L)) (cdr L))]
     [else (cons (car L) (pass1 (cdr L)))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion pass
+;Dominio: Lista
+;Recorrido: Lista (game)
+;Descripcion: junta el aumento del turno con el resto del game
+;Tipo: Declarativa
 (define (pass L)
   (cons (pass1 (car L)) (cdr L)))
 
 ;finish-------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
-(define (encontFinish L e)           ;encuentra y elimina el elemento de la lista
+;Funcion encontFinish
+;Dominio: Lista X String
+;Recorrido: Lista 
+;Descripcion: encuentra y elimina el elemento de la lista
+;Tipo: Natural
+(define (encontFinish L e)          
   (cond
     [(empty? L) null]
     [(equal? (car(car L)) e) (cdr L)]
     [else (cons (car L) (encontFinish (cdr L) e))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion reordenar
+;Dominio: Lista X Entero X Entero
+;Recorrido: "String"
+;Descripcion: Regresa el jugador con la mayor puntuación
+;Tipo: Cola
 (define (reordenar L e a)
   (cond
     [(empty? L) a]
     [(>= (car(cdr(cdr(car L)))) e) (reordenar (cdr L) (car(cdr(cdr(car L)))) (car(car L)))]
     [else (reordenar (cdr L) e a)]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion finish1
+;Dominio: Lista X Entero
+;Recorrido: Lista
+;Descripcion: Regresa a los jugadores por odern de puntaje 
+;Tipo: Natural
 (define (finish1 L i)
   (cond
     [(empty? L) null]
     [else (cons (list (reordenar L 0 0) i) (finish1 (encontFinish  L (reordenar L 0 0)) (+ i 1)))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion finish
+;Dominio: Lista 
+;Recorrido: Lista
+;Descripcion: regresa la tabla de posiciones de los jugadores
+;Tipo: Declarativa
 (define (finish L)
   (finish1 (car L) 1))
 ;Play----------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion Play 
+;Dominio: Lista X Funcion (o null)
+;Recorrido: Lista (Game)
+;Descripcion: funcion de jugar, realiza la accion segun la funcion establecida
+;Tipo: Declarativa
 (define play (lambda (L action)
                (cond
                  [(equal? action null) (nulo L)]
@@ -234,35 +236,34 @@
                  [else (action L)])))
 
 ;TDA game - status-------------------------------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion status
+;Dominio: Lista
+;Recorrido: Lista
+;Descripcion: Regresa el estado actual del juego
+;Tipo: Declarativa
 (define (status L)
   L)
 
 ;TDA game - score--------------------------------------------------------------------------------------------------
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion score1
+;Dominio: Lista X String
+;Recorrido: Entero
+;Descripcion: Regresa el puntaje de un jugador seleccionado
+;Tipo: Cola
 (define (score1 L name)
   (cond
     [(empty? L) null]
     [(equal? (car(car L)) name) (car(cdr(cdr(car L))))]
     [else (score1 (cdr L) name)]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion score
+;Dominio: Lista X String
+;Recorrido: Entero
+;Descripcion: Llama a la funcion anterior para encontrar el puntaje de cierto jugador
+;Tipo: Declarativa
 (define (score L name)
   (score1 (car L) name))
   
-;TDA game - game->string-------------------------------------------------------------------------------------------
 
 
 
@@ -270,9 +271,8 @@
 
 
 (define mazo (cardsSet ListS 8 -1 #t))
-(define game1 (game 4 mazo stackMode random))
+(define game1 (game 3 mazo stackMode random))
 (define game2 (register "juanito" game1))
 (define game3 (register "carlitos" game2))
 (define game4 (register "manolito" game3))
-(define game5 (register "pedrito" game4))
 
--- GenerarCards_20814010-8_MarchantOsorio.rkt
+++ GenerarCards_20814010-8_MarchantOsorio.rkt
@@ -10,80 +10,84 @@
 (define ListN(list "1 " "2 " "3 " "4 " "5 " "6 " "7 " "8 " "9 " "10 " "11 " "12 " "13 " "14 " "15 " "16 " "17 " "18 " "19 " "20 " "21 " "22 " "23 " "24 " "25 " "26 " "27 " "28 " "29 " "30 "
                    "31 " "32 " "33 " "34 " "35 " "36 " "37 " "38 " "39 " "40 " "41 " "42 " "43 " "44 " "45 " "46 " "47 " "48 " "49 " "50 " "51 " "52 " "53 " "54 " "55 " "56 " "57 "))
 
+;Generacion de 1 Carta------------------------------------------------------------------------
 ;Funcion C1
 ;Dominio: entero X entero
 ;Recorrido: List
-;Descripcion: se usa para generar las n primeras cartas
+;Descripcion: recibe un numero y una constante = 1, y se usa para generar las n primeras cartas
 ;Tipo: Natural
 (define (C1 n i)
   (cond
-    [(<= i n) (cons i (C1 n (+ i 1)))]
-    [else null]))
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
-;se usa para generar por filas de tamaño n,numeros consecutivos 
+    [(<= i n) (cons i (C1 n (+ i 1)))]   ;agrega con recursion natural numeros de 1 hasta n
+    [else null]))                        ;caso fin
+
+;Generacion de N cartas-----------------------------------------------------------------------
+;Funcion C2
+;Dominio: Entero X Entero X Entero
+;Recorrido: Lista
+;Descripcion: recibe un numero y 2 constates = 1, y genera una carta sin el primer elemento
+;Tipo: Cola
 (define (C2 n j k)
   (cond
-    [(<= k n) (cons (+(* n j)(+ k 1))(C2 n j (+ k 1)))]
+    [(<= k n) (cons (+(* n j)(+ k 1))(C2 n j (+ k 1)))]   ;genera un listado con recursion
     [else null]))
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+
+;Funcion C3
+;Dominio:  Entero X Entero X Entero
+;Recorrido: Lista de Listas
+;Descripcion: recibe un numero n y 2 constantes = 1, y genera n cartas.
+;Tipo: Cola
 (define (C3 n j k)
   (cond
-    [(<= j n)(cons (cons 1 (C2 n j k))(C3 n (+ j 1) k))]
+    [(<= j n)(cons (cons 1 (C2 n j k))(C3 n (+ j 1) k))]          ;agrega las cartas anteriores a una nueva lista
     [else null]))
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
-;se usa para generar numeros consecutivos por columnas de tamaño n
+
+;Generacion de las N*N Cartas-------------------------------------------------------------
+;Funcion C4
+;Dominio: Entero X Entero X Entero X Entero
+;Recorrido: Lista
+;Descripcion: recibe un numero y 3 constates = 1, y genera una carta sin el primer elemento
+;Tipo: Cola
 (define (C4 n i j k)
   (cond
     [(<= k n)(cons (+(+ (+ (modulo(-(+(*(- i 1)(- k 1))j)1)n) (* n(- k 1))) 2) n) (C4 n i j (+ k 1)))]
     [else null]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion C5
+;Dominio: Entero X Entero X Entero X Entero
+;Recorrido: Lista de listas
+;Descripcion: recibe un numero y 3 constates = 1, y genera una parte del set de cartas de tamaño n
+;Tipo: Cola
 (define (C5 n i j k)
   (cond
     [(<= j n)(cons(cons (+ i 1)(C4 n i j k))(C5 n i (+ j 1) k))]
     [else null]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion C6
+;Dominio: Entero X Entero X Entero X Entero
+;Recorrido: Lista de Listas
+;Descripcion: recibe un numero y 3 constates = 1, y genera un mazo de cartas completo
+;Tipo: Cola
 (define (C6 n i j k)
   (cond
     [(<= i n)(append(C5 n i j k)(C6 n (+ i 1) j k))]
     [else null]))
 
 ;;;;;;;;;;;;; TDA cardsSet - constructor ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion Cards
+;Dominio: Entero
+;Recorrido: Lista de Listas
+;Descripcion: a partir de un N que representa la cantidad de elementos por cartas, genera un mazo completo
+;Tipo: Cola
 ;main principal para la función
 (define Cards(lambda(n)
           (append(cons(C1 n 1)(C3 (- n 1) 1 1))(C6 (- n 1) 1 1 1))))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion cardSet2 (recortar segun max)
+;Dominio: Lista X Entero X Entero
+;Recorrido: Lista de Listas (o si Max = 1, Lista)
+;Descripcion: recorta la lista en en la posicion que se desee
+;Tipo: Cola
 (define cardSet2 (lambda(L Num Max)   ;suponiendo que se basara meramente en numeros L = null
                  (cond
                    [(> Max 0)(cons(car L)(cardSet2 (cdr L) Num (- Max 1)))]
@@ -91,94 +95,94 @@
                    [else null])))
 
 ;;;;;;;;;;;;;;;;       Simbologia        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion recorrer
+;Dominio: Lista X Entero X Entero
+;Recorrido: String
+;Descripcion: recorre la lista de simbolos, y retorna el elemento de la posicion indicada
+;Tipo: Cola 
 (define (recorrer Ls n acum)
   (cond
     [(empty? Ls) n] ;se puede poner null para que no exista el numero o se puede poner n para que este el numero.
     [( = acum (- n 1)) (car Ls)]
     [else (recorrer (cdr Ls) n (+ acum 1))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo:        
+;Funcion igualar
+;Dominio: Lista X Lista
+;Recorrido: Lista
+;Descripcion: a base de una carta, crea una carta de la lista de simbolos
+;Tipo: Cola        
 (define (igualar Ls Lc)
   (cond
     [(empty? Lc) null]
-    [else (cons (recorrer Ls (car Lc) 0) (igualar Ls (cdr Lc)))]))
+    [else (cons (recorrer Ls (car Lc) 0) (igualar Ls (cdr Lc)))])) ;agrega simbolo x simbolos hasta llenar la carta
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion Symbo
+;Dominio: Lista X Lista
+;Recorrido: Lista de Listas
+;Descripcion: genera una lista de cartas con simbolos
+;Tipo: Cola
 (define (Symbo Ls Lc)
   (cond
     [(empty? Lc) null]
-    [else (cons(igualar Ls (car Lc))(Symbo Ls (cdr Lc)))]))
+    [else (cons(igualar Ls (car Lc))(Symbo Ls (cdr Lc)))]))  ;agrega carta por carta hasta llenar el mazo
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion cardsSet3
+;Dominio: Lista X Entero X Entero
+;Recorrido: Lista de Listas 
+;Descripcion: bajo los parametros de entrada genera un set de cartas
+;Tipo: Declarativa 
 (define cardsSet3 (lambda(L Num Max)
                   (define L1 (Cards Num))
                   (cardSet2 L1 Num Max)))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion cardsSet4 
+;Dominio: Lista X Entero X Entero
+;Recorrido: Lista de Listas 
+;Descripcion: remplaza la lista de números por la lista de string ingresada
+;Tipo: Declarativa
 (define cardsSet4 (lambda(L Num Max)
                    (cond
                      [(empty? L) (cardsSet3 L Num Max)]                ;(cardsSet null 3 -1)
                      [else (Symbo L (cardsSet3 L Num Max))])))         ;(cardsSet (list "a" "b" "c" "d" "e" "f" "g") 3 -1)
 ;;;;;;;;;;;;;;;;;;;;  random ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
+;Funcion encotrar 
+;Dominio: Lista X Entero
 ;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Descripcion: recorre la lista hasta encontrar el elemento por su posicion y lo devuelve
+;Tipo: Cola
 (define (encontrar L n)
   (cond
     [(empty? L) null]
     [(equal? n 1)(car L)]
     [else (encontrar (cdr L) (- n 1))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion eleminar
+;Dominio: Lista X Entero
+;Recorrido: Lista
+;Descripcion: Recorre la lista hasta encontrar un elemento por su posicion y lo borra
+;Tipo: Natural
 (define (eleminar L n)
   (cond
     [(empty? L) null]
     [(equal? n 1)(cdr L)]
     [else (cons (car L) (eleminar (cdr L) (- n 1)))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion randomFn
+;Dominio: Lista X Entero
+;Recorrido: Lista 
+;Descripcion: desordena elementos de una lista
+;Tipo: Natural
 (define (randomFn L NR)
   (cond
     [(equal? (length L) 1) (cons (car L) null)]
     [else (cons (encontrar L NR) (randomFn (eleminar L NR) (random 1 (+(length (eleminar L NR))1))))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion cardsSet
+;Dominio: Lista X Entero X Entero X Booleano
+;Recorrido: Lista de Listas
+;Descripcion: genera el conjunto de cartas completo con una lista de elementos, cantidad de elementos, numero max de cartas y si se quiere al azar o no
+;Tipo: Declarativa
 (define (cardsSet L N max FRandom)  ;random = #true / #false
   (cond
     [(equal? FRandom #true) (randomFn (cardsSet4 L N max) (random 1 (+ (length (cardsSet4 L N max))1)))]
@@ -186,57 +190,56 @@
   
 
 ;;;;;;;;;;;;; TDA cardsSet - numCards ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
-;preferi generar mi popio length
+;Funcion loopNc
+;Dominio: Lista X Entero
+;Recorrido: Entero
+;Descripcion: Cuenta la cantidad de elementos en una lista (preferi generar mi popio length)
+;Tipo: Cola
 (define (loopNc L i)
   (cond
     [(empty? L) i]
     [else (loopNc (cdr L)(+ i 1))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion numCards
+;Dominio: Lista
+;Recorrido: Entero
+;Descripcion: Regresa la cantidad de elementos en una lista
+;Tipo: Declarativa
 (define (numCards L)
   (loopNc L 0))
 
 ;;;;;;;;;;;;;;; TDA cardsSet - nthCard ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion loopNth
+;Dominio: Lista X Entero X Entero
+;Recorrido: Elemento (o una Lista)
+;Descripcion: regresa el elemento que se encuentre en sierta posición
+;Tipo: Cola
 (define (loopNth L j i)
   (cond
     [(empty? L) null]                      ;Caso lista vacia o fuera del alcance
     [(= (- j 1) i) (car L)]                ;Devuelve la carta solicitada
     [else (loopNth (rest L) j (+ i 1))]))  ;Cumple el loop sumando 1 a i
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion nthCard
+;Dominio: Lista X Entero 
+;Recorrido: Lista
+;Descripcion: regresa la carta que se encuentre en la posicion j
+;Tipo: Declarativa
 (define (nthCard L j)                      ;Funcion principal nthCard, L es lista; j el numero
   (loopNth L j 0))                         ;Envia los datos al loop
 
 ;;;;;;;;;;;;;;; TDA cardsSet - findTotalCards ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion findTotalCards
+;Dominio: Lista
+;Recorrido: Entero
+;Descripcion: A partir de una carta de muestra, determina la cantidad total de cartas que se deben producir para construir un conjunto válido.
+;Tipo: Declarativa
 (define (findTotalCards L)
   (+(*(- (length L )1)(- (length L)1))(length L)))  
 
 ;;;;;;;;;;;;;;; TDA cardsSet - Dobble? ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Funcion buscar
-;Dominio: Listas y un entero
+;Dominio: Lista X Lista X Lista X Entero
 ;Recorrido: Entero
 ;Descripcion entrega el numero de elementos repetidos que hay entre ambas listas.
 ;Tipo: Cola
@@ -248,10 +251,10 @@
     [else (buscar(cdr L1) L2 L12 acum)]))
 
 ;Funcion buscar3
-;Dominio: Listas
+;Dominio: Lista X Lista X Lista
 ;Recorrido: Booleano
 ;Descripcion recorre ambas listas y las envia a buscar. Solo 1 elemento en comun, de esta forma ve que el n-1 sea primo o un numero elevado a un primo.
-;Tipo: 
+;Tipo: Cola
 (define (buscar3 L1 L2 L22)
   (cond
     [(empty? (cdr L1)) #true]
@@ -261,10 +264,10 @@
 
 
 ;Funcion buscar2
-;Dominio: Listas
+;Dominio: Lista X Lista
 ;Recorrido: Booleano
 ;Descripcion: no elentos repetido en una misma carta.
-;Tipo: 
+;Tipo: Cola
 (define (buscar2 L1 L2)
   (cond
     [(empty? L1) #true]
@@ -275,7 +278,7 @@
 ;Dominio: Listas
 ;Recorrido: Booleano
 ;Descripcion: misma cantidad de elementos.
-;Tipo:
+;Tipo:Cola
 
 (define (buscar4 L)
   (cond
@@ -284,10 +287,10 @@
     [else #false]))
 
 ;Funcion Dobble?
-;Dominio: Listas
+;Dominio: Lista
 ;Recorrido: Booleano
-;Descripcion: 
-;Tipo:
+;Descripcion: Recopilacion de anteriores y define si es un conjunto valido o no
+;Tipo: Declarativa
 (define (Dobble? L)
   (cond
     [(buscar4 L)(cond
@@ -299,21 +302,20 @@
 
 
 ;;;;;;;;;; TDA cardsSet - requiredElements  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion requiredElements
+;Dominio: Lista
+;Recorrido: Entero
+;Descripcion: A partir de una carta de muestra, determina la cantidad total de elementos para un mazo 
+;Tipo: Declarativa
 (define (requiredElements L)
   (findTotalCards L))
 
-
-;;;;;;;;;; TDA cardsSet - missingCards  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo:
+;;;;;;;;;; TDA cardsSet - missingCards  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;(RecoMiss (cardsSet ListN 3 -1 #t) (cardsSet ListN 3 4 #t) (cardsSet ListN 3 4 #t))
+;Funcion RecoMiss
+;Dominio: Lista X Lista X Lista
+;Recorrido: Listas de listas
+;Descripcion: regresa la cantidad de cartas para que el conjunto sea valido
+;Tipo: Natural
 (define (RecoMiss L1 L2 L22)
   (cond
     [(empty? L1) null]                                              ;cuando la lista se acaba 
@@ -321,62 +323,62 @@
     [(equal? (car L1)(car L2))(RecoMiss (cdr L1) L22 L22)]          ;cuando el elemento si esta en la lista se descarta y sigue
     [else (RecoMiss L1 (cdr L2) L22)]))                             ;caso de avance al siguiente elemento de la lista
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion missingCards L
+;Dominio: Lista (cartas)
+;Recorrido: Lista de Listas
+;Descripcion: a base de un conjunto de cartas, te entrega el resto de estas 
+;Tipo: Declarativa
 (define (missingCards L)
   (cond
     [(>= (buscar (car(cardsSet ListN (numCards (car L)) -1)) (car L) (car(cardsSet ListN (numCards (car L)) -1)) 0) 1) (RecoMiss(cardsSet ListN (numCards (car L)) -1) L L)]
     [else (RecoMiss(cardsSet ListS (numCards (car L)) -1) L L)]))
 
 ;;;;;;;;;; TDA cardsSet - CardsSet->String  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion CardsSet->String2
+;Dominio: Lista 
+;Recorrido: Lista
+;Descripcion: transforma cada carta en un string
+;Tipo: Natural
 (define (CardsSet->String2 L)
   (cond
     [(empty? L) null]
     [else (cons (apply string-append (car L))(CardsSet->String2 (cdr L)))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion putNCard
+;Dominio: Lista X Entero
+;Recorrido: Lista
+;Descripcion: agrega elementos para el aspecto de cada carta
+;Tipo: Natural
 (define (putNCard L i)
   (cond
     [(empty? L) null]
     [else (cons(append (list "Card " (number->string  i) ": " (car L)) (list "\n")) (putNCard (cdr L) (+ i 1)))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion CardsSet->String
+;Dominio: Lista
+;Recorrido: String
+;Descripcion: transforma la Lista de Strings genera anteriormente en un String
+;Tipo: Declarativa
 (define (CardsSet->String L)
   (apply string-append(CardsSet->String2 (putNCard (CardsSet->String2 L) 1))))
 
 
 ;;;;;;;;;; TDA cardsSet - addCard ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion addCard2
+;Dominio: Lista X Lista
+;Recorrido: Lista de Listas
+;Descripcion: agrega una carta a la lista
+;Tipo: Natural
 (define (addCard2 L c)
   (cond
     [(empty? L) (cons c null)]
     [else (cons (car L) (addCard2 (cdr L) c))]))
 
-;Funcion 
-;Dominio: 
-;Recorrido: 
-;Descripcion: 
-;Tipo: 
+;Funcion addCard
+;Dominio: Lista X Lista
+;Recorrido: Lista de Listas
+;Descripcion: Comprueba si el set con la carta nueva es valido, si lo es la agrega y si no lo es no la agrega
+;Tipo: Declarativa
 (define (addCard L c)
   (cond
     [(Dobble? (addCard2 L c)) (addCard2 L c)]
